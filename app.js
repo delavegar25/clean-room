3913d1cefd27aa3050a2f3db103423b595061435
Pascal <pascalokereke18@gmail.com>
2023-12-17 14:54:01 +0100

    manipulate the elements

---
+++ app.js
@@ -0,0 +1,15 @@
+function app() {
+// onclick, hide the empty state(checkbox)
+// show the spinner
+// 3 seconds later, show the checked icon
+// get the button and icons
+
+
+const checkboxButton = document.querySelector('#shopping-item-checkbox');
+
+
+
+
+}
+
+app();
